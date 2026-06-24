5dfed321d62ccf0ce0ab7d4b9400370c6924ca9b
Jesse Sorman <jessesorman@gmail.com>
2026-06-24 15:10:39 -0400

    refactor favorites_spec.rb

--- spec/requests/favorites_spec.rb
+++ spec/requests/favorites_spec.rb
@@ -2,18 +2,17 @@ require './spec/spec_helper.rb'
 
 RSpec.describe 'Markets', type: :request do
   before(:each) do
-    @market_1_id = create(:market).id
-    @market_2_id = create(:market).id
-    @market_3_id = create(:market).id
-    @market_4_id = create(:market).id
-    @market_5_id = create(:market).id
+    @market_1 = create(:market)
+    @market_2 = create(:market)
+    @market_3 = create(:market)
+    @market_4 = create(:market)
+    @market_5 = create(:market)
   end
 
   describe 'find favorites' do
     it 'gets users favorites' do
 
       user = create(:user, email: 'johndoe@example.com', name: 'John Doe', password: 'password123')
-      market = create(:market)
 
       payload = {
         email: 'johndoe@example.com',
@@ -26,7 +25,7 @@ RSpec.describe 'Markets', type: :request do
 
       payload = {
         user: user.id,
-        market: market.id
+        market: @market_1.id
       }
 
       post '/api/favorites', payload.to_json, {
@@ -34,10 +33,11 @@ RSpec.describe 'Markets', type: :request do
         'HTTP_AUTHORIZATION' => "Bearer #{token}"
       }
 
-      get '/markets/favorites', market_ids: [market.id]
+      get '/markets/favorites'
 
       expect(last_response).to be_successful
-      expect(user.reload.favorite_markets).to include(market)
+      expect(user.reload.favorite_markets).to include(@market_1)
+      expect(user.reload.favorite_markets).not_to include(@market_2)
     end
   end
 end
