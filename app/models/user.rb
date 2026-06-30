cff811522755456483dc0096ecc8d3adceaaf397
Jesse Sorman <jessesorman@gmail.com>
2026-06-30 15:30:40 -0400

    adds additional validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,18 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 8 }, format: { 
+  before_validation { email&.downcase!&.strip! }
+
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password,
+  presence: true,
+  length: { minimum: 8 },
+  format: {
     with: /\A(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])/,
-    message: "must include at least one lowercase letter, one uppercase letter, one number, and one special character" 
-  }, 
-  if: -> { password.present? }, on: :create
+    message: "must include at least one lowercase letter, one uppercase letter, one number, and one special character"
+  },
+  if: -> { password.present? },
+  on: :create
 
   has_many :user_favorites, dependent: :destroy
   has_many :favorite_markets, through: :user_favorites, source: :market
--- app/models/user_favorite.rb
+++ app/models/user_favorite.rb
@@ -4,5 +4,7 @@ class UserFavorite < ActiveRecord::Base
   belongs_to :user
   belongs_to :market
 
+  validates :user_id, presence: true
+  validates :market_id, presence: true
   validates :user_id, uniqueness: { scope: :market_id, message: "you've already favorited this market" }
 end
