a5b4a684c5479668ee1fcd1275ba28606ab171a1
Jesse Sorman <jessesorman@gmail.com>
2026-07-12 17:29:06 -0600

    refactor config.rb handler

--- api/config.rb
+++ api/config.rb
@@ -2,26 +2,30 @@ require_relative '../config/environment'
 require_relative 'app'
 
 Handler = Proc.new do |req|
+  request_hash = req || {}
+  headers = request_hash['headers'] || {}
+  body = request_hash['body'].to_s
+
   env = {
-    'REQUEST_METHOD' => req['method'],
-    'PATH_INFO' => req['path'],
-    'QUERY_STRING' => req['query'].to_s,
+    'REQUEST_METHOD' => request_hash['method'] || 'GET',
+    'PATH_INFO' => request_hash['path'] || '/',
+    'QUERY_STRING' => request_hash['query'].to_s,
     'SERVER_NAME' => 'localhost',
     'SERVER_PORT' => '80',
-    'rack.input' => StringIO.new(req['body'].to_s),
+    'rack.input' => StringIO.new(body),
     'rack.errors' => $stderr,
-    'HTTP_HOST' => req['headers']['host'].to_s,
-    'HTTP_ORIGIN' => req['headers']['origin'].to_s,
-    'HTTP_AUTHORIZATION' => req['headers']['authorization'].to_s,
-    'CONTENT_TYPE' => req['headers']['content-type'].to_s,
-    'CONTENT_LENGTH' => req['body'].to_s.bytesize.to_s
+    'HTTP_HOST' => headers['host'].to_s,
+    'HTTP_ORIGIN' => headers['origin'].to_s,
+    'HTTP_AUTHORIZATION' => headers['authorization'].to_s,
+    'CONTENT_TYPE' => headers['content-type'].to_s,
+    'CONTENT_LENGTH' => body.bytesize.to_s
   }
 
-  status, headers, body = APP.call(env)
+  status, headers_out, body_out = APP.call(env)
 
   {
     'statusCode' => status,
-    'headers' => headers,
-    'body' => body.each.to_a.join
+    'headers' => headers_out,
+    'body' => body_out.each.to_a.join
   }
 end
