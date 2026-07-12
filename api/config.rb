903a829bca2d43be3ab6732dedc40244b39ef639
Jesse Sorman <jessesorman@gmail.com>
2026-07-12 17:22:53 -0600

    refactor config.rb handler

--- api/config.rb
+++ api/config.rb
@@ -2,11 +2,24 @@ require_relative '../config/environment'
 require_relative 'app'
 
 Handler = Proc.new do |req, res|
-  status = 200
-  headers = { 'Content-Type' => 'application/json' }
-  body = { message: 'ok' }.to_json
+  env = {
+    'REQUEST_METHOD' => req.method,
+    'PATH_INFO' => req.path,
+    'QUERY_STRING' => req.query_string.to_s,
+    'SERVER_NAME' => 'localhost',
+    'SERVER_PORT' => '80',
+    'rack.input' => StringIO.new(req.body.to_s),
+    'rack.errors' => $stderr,
+    'HTTP_HOST' => req.headers['host'].to_s,
+    'HTTP_ORIGIN' => req.headers['origin'].to_s,
+    'HTTP_AUTHORIZATION' => req.headers['authorization'].to_s,
+    'CONTENT_TYPE' => req.headers['content-type'].to_s,
+    'CONTENT_LENGTH' => req.body.to_s.bytesize.to_s
+  }
+
+  status, headers, body = APP.call(env)
 
   res.status = status
   headers.each { |k, v| res[k] = v }
-  res.body = body
+  res.body = body.each.to_a.join
 end
