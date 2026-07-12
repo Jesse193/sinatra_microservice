14918b29106e048b20fbd99ffe57c3eaa73b2b8e
Jesse Sorman <jessesorman@gmail.com>
2026-07-12 17:14:23 -0600

    add handler to config.rb

--- api/config.rb
+++ api/config.rb
@@ -1,4 +1,12 @@
 require_relative '../config/environment'
 require_relative 'app'
 
-APP
+Handler = Proc.new do |req, res|
+  status = 200
+  headers = { 'Content-Type' => 'application/json' }
+  body = { message: 'ok' }.to_json
+
+  res.status = status
+  headers.each { |k, v| res[k] = v }
+  res.body = body
+end
