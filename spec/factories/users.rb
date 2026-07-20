00a5be591ec4f56cc5d58ba097a53f10d9da4006
Jesse Sorman <jessesorman@gmail.com>
2026-07-20 14:48:33 -0400

    refactor user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :user do
     name { "John Doe" }
     email { "email@email.com" }
-    password_digest { "Pa55w0!rdis5ecureandlong" }
+    password { "Pa55w0!rdis5ecureandlong" }
+    password_confirmation { "Pa55w0!rdis5ecureandlong" }
   end
 end
