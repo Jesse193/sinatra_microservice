f6a6fe8dcc2158e2f6691656770319f07f97fc3c
Jesse Sorman <jessesorman@gmail.com>
2026-07-20 15:13:26 -0400

    add logout test

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,15 +1,15 @@
 require "spec_helper"
 
 RSpec.describe "Login", type: :feature do
-  let!(:user) { create(:user) }
+  let!(:users) { create_list(:user, 5) }
 
   it "logs in via the React UI" do
     visit "/"
 
     click_button "Accept"
 
-    fill_in "email", with: user.email
-    fill_in "password", with: user.password
+    fill_in "email", with: users[0].email
+    fill_in "password", with: users[0].password
 
     click_button "Login"
     expect(page).to have_content("Welcome")
@@ -20,10 +20,25 @@ RSpec.describe "Login", type: :feature do
 
     click_button "Accept"
 
-    fill_in "email", with: user.email
+    fill_in "email", with: users[0].email
     fill_in "password", with: "wrongpassword"
 
     click_button "Login"
     expect(page).to have_content("Invalid email or password")
+  end
+
+  it "logs out via the React UI" do
+    visit "/"
+
+    click_button "Accept"
+
+    fill_in "email", with: users[0].email
+    fill_in "password", with: users[0].password
+
+    click_button "Login"
+    expect(page).to have_content("Welcome")
+
+    click_button "Logout"
+    expect(page).to have_content("Login")
   end
 end
